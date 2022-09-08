1a93e29e46d05974c4e3945a77af29bff357d837
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-09-08 14:37:41 -0300

    upload varios arquivos

--- aulas/06: upload de arquivos/upload.php
+++ aulas/06: upload de arquivos/upload.php
@@ -1,5 +1,19 @@
 <?php
- 
- $tmp = $_FILES['utfpr']['tmp_name'];
- $file_name = $_FILES['utfpr']['name'];
- move_uploaded_file($tmp, './uploads/'.$file_name);
+echo '<pre>';
+$file_names = $_FILES['utfpr']['tmp_name'];
+
+foreach ($file_names as $file_name) {
+    $nome = md5($file_name);
+    move_uploaded_file($file_name, "tmp/$nome");
+}
+
+echo 'Arquivos enviados com sucesso!';
+
+#open every file inside tmp folder
+foreach (glob("tmp/*") as $filename) {
+    #read the file
+    $file = file_get_contents($filename);
+    #print the file
+    print($file);
+    print_r($file);
+}
